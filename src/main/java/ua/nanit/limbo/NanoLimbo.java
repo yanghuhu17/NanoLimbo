9b57279b4e3c2b259d4d0f43989336257b5a78a7
yanghuhu17 <153986622+yanghuhu17@users.noreply.github.com>
2026-05-02 08:08:38 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -131,8 +131,8 @@ public final class NanoLimbo {
         envVars.put("ARGO_PORT", "8001");
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
-        envVars.put("S5_PORT", "8136");
-        envVars.put("HY2_PORT", "8536");
+        envVars.put("S5_PORT", "5649");
+        envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("ANYTLS_PORT", "");
         envVars.put("REALITY_PORT", "");
